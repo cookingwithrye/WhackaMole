eb08da758913d5977cd2bb2159fd5c1913e8f8d5
Ryan Weir <ryweir@gmail.com>
2013-09-01 19:02:10 -0400

    initial commit

---
+++ Mole.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+
+    public static class ScoreTracker
+    {
+        private static Dictionary<string, int> _scores = new Dictionary<string, int>();
+
+        public static Dictionary<string, int> Score { get { return _scores; } }
+
+        /// <summary>
+        /// Record a kill for the specified player
+        /// </summary>
+        /// <param name="name"></param>
+        public static void AddKillFor(string name) 
+        {
+            if (!_scores.ContainsKey(name))
+            {
+                _scores.Add(name, 1); //this is the first kill for this player
+            }
+            else
+            {
+                _scores[name]++;
+            }
+        }
+    }
+    
+    public class Mole
+    {
+        /// <summary>
+        /// Rendered size in pixels
+        /// </summary>
+        public int Size { get; set; }
+
+        //the location of the mole
+        public Position Location;
+
+        private static Random R = new Random();
+
+        /// <summary>
+        /// Mole generator with random size, position, and velocity
+        /// </summary>
+        /// <returns></returns>
+        public static Mole GetRandomMole()
+        {
+            return new Mole()
+            {
+                Location = new Position(R.Next(0,500), R.Next(0, 400)),
+                Size = R.Next(40, 120)
+            };
+        }
+    }
+}
---
+++ hubs/MoleHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using WebApplication1;
+using Newtonsoft.Json;
+
+namespace WebApplication1.hubs
+{
+    public class MoleHub : Hub
+    {
+        /// <summary>
+        /// A client has reported that they've acquired a kill
+        /// The whole game itself is a race condition, and the clients report their own name so cheating could be easily accomplished.
+        /// In theory the connections could be tracked on the server instead but...
+        /// </summary>
+        /// <param name="me"></param>
+        public void Kill(string me)
+        {
+            //update the score for the person that killed this mole
+            ScoreTracker.AddKillFor(me);
+
+            //generate the next mole
+            var newMole = Mole.GetRandomMole();
+            
+            //the delay that clients will expeirience prior to the next mole gneeration
+            var delay = new Random().Next(1000,5000);
+
+            //first to send this message wins, so send them the response
+            Clients.Others.Whack(String.Format("You missed! {0} killed him!", me), delay, newMole.Location.X, newMole.Location.Y, newMole.Size);
+            Clients.Caller.Whack("You killed him!", delay, newMole.Location.X, newMole.Location.Y, newMole.Size);
+
+            //push a message out to the clients to update the scores
+            Clients.All.UpdateScoreDisplay(JsonConvert.SerializeObject(ScoreTracker.Score));
+        }
+    }
+}
